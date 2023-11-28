309e9cc9890757d8714fb1245d72f29db37cd386
Dulari Kulathunga <138515823+dulari2023@users.noreply.github.com>
2023-11-27 22:39:43 -0600

    Update pizza_order_details.sql

--- SQL/pizza_order_details/pizza_order_details.sql
+++ SQL/pizza_order_details/pizza_order_details.sql
@@ -26,3 +26,37 @@ SELECT
   *
 FROM 
   `case-studies-405816.Pizza_info.pizza_types` 
+
+
+-- 1. Find the total quantity of each pizza ordered.
+
+SELECT
+  pizza_id,
+  SUM(quantity) AS total_quantity
+FROM
+  `case-studies-405816.Pizza_info.order_info`
+GROUP BY
+  pizza_id
+
+
+-- 2. Which type of pizza (pizza_id) was ordered on 2015.12.31?
+
+SELECT
+ DISTINCT(pi.pizza_id)
+FROM
+ `case-studies-405816.Pizza_info.order_info` AS pi
+JOIN
+ `case-studies-405816.Pizza_info.order_dates` od ON pi.order_id = od.order_id
+WHERE
+ od.date = '2015-12-31';
+
+
+-- 3. What is the most popular pizza type?
+
+
+
+
+
+
+
+
