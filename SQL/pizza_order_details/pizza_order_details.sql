52a9f6e2b73697fcd13c5ac9dfead9528ed0e3a4
Dulari Kulathunga <138515823+dulari2023@users.noreply.github.com>
2023-11-27 22:32:34 -0600

    Update pizza_order_details.sql

--- SQL/pizza_order_details/pizza_order_details.sql
+++ SQL/pizza_order_details/pizza_order_details.sql
@@ -1,1 +1,28 @@
+-- Let's start by introducing the tables.
 
+-- order_info table
+SELECT 
+  *
+FROM 
+  `case-studies-405816.Pizza_info.order_info` 
+
+
+-- order_dates table
+SELECT 
+  *
+FROM 
+  `case-studies-405816.Pizza_info.order_dates` 
+
+
+-- order_dates table
+SELECT 
+  *
+FROM 
+  `case-studies-405816.Pizza_info.pizza_prices` 
+
+
+-- order_dates table
+SELECT
+  *
+FROM 
+  `case-studies-405816.Pizza_info.pizza_types` 
